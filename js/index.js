4c907c130ec5760ee3d85dead082943e4dedee05
Roger Bif <rogerbif@gmail.com>
2019-07-18 00:06:04 -0300

    New updates

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 var fileInput = document.getElementById('fileInput');
 var fileDisplayArea = document.getElementById('fileDisplayArea');
+var t0,t1;
 
 fileInput.addEventListener('change', function(e) {
+    //dispara o contador de tempo
+    t0 = performance.now();
+
     var file = fileInput.files[0];
     var textType = /text.*/;
     
@@ -10,61 +14,49 @@ fileInput.addEventListener('change', function(e) {
         
         reader.onload = function(e) {
             var contentInicial = reader.result;
-            
-            //mostra conteudo do arquivo de texto
-            fileDisplayArea.textContent = contentInicial;
-
-            contentInicial = removeAcentos(contentInicial);
-            document.getElementById("demo0").innerHTML = "removeAcentos: " + '<br>' + contentInicial;
-
-            contentInicial = removeCaracEsp(contentInicial);
-            document.getElementById("demo1").innerHTML = "removeCaracEsp: " + '<br>' +  contentInicial;
-
-            contentInicial = transMaiusc(contentInicial);
-            document.getElementById("demo2").innerHTML = "transMaiusc: " + '<br>' +  contentInicial;
-
-            contentInicial = removeEspacos(contentInicial);
-            document.getElementById("demo3").innerHTML = "removeEspacos: " + '<br>' + contentInicial.slice(0,135) + '<br>' + contentInicial.slice(135,270) + '<br>' + contentInicial.slice(270,405) + '<br>' + contentInicial.slice(405,540);
-
-            var content1 = separaPartes(contentInicial);
-            document.getElementById("demo4").innerHTML = "separaPartes: " + '<br>' +  content1;
-
-            var content2 = separaPartes(contentInicial.substring(1));
-            document.getElementById("demo5").innerHTML = "separaPartes2: "  + '<br>' +  content2;
-            
-            contentInicial = content1.concat(content2);
-            document.getElementById("demo6").innerHTML = "concat: "  + '<br>' + contentInicial;
-
-            var content3 = contentInicial.filter( onlyUnique );
-            document.getElementById("demo7").innerHTML = "onlyUnique: "  + '<br>' + content3;
-
-            var contentFinal2 = percorreLista2(contentInicial, content3);
-            document.getElementById("demo10").innerHTML = "percorreLista2: " + contentFinal2; 
+            Gerenciador(contentInicial);
         }
-        
         reader.readAsText(file);	
     } else {
         fileDisplayArea.innerText = "File not supported!"
     }
 });
 
-function removeEspacos (str){
-    str = str.replace(/\s/g, '');
-    return str;
-}
+function Gerenciador (contentInicial){
 
-function contaOcorrencias(str,stringsearch) {
+    //mostra conteudo do arquivo de texto
+    fileDisplayArea.textContent = contentInicial;
 
-    for(var i=count=0; i<str.length; count+=+(stringsearch===str[i++]));
+    contentInicial = removeAcentos(contentInicial);
+    document.getElementById("demo0").innerHTML = "removeAcentos: " + contentInicial.length + '<br>' + contentInicial;
 
-    return stringsearch + " " + count + '<br>';
-}
+    contentInicial = removeCaracEsp(contentInicial);
+    document.getElementById("demo1").innerHTML = "removeCaracEsp: " + '<br>' +  contentInicial;
 
-//https://stackoverflow.com/questions/6259515/javascript-elegant-way-to-split-string-into-segments-n-characters-long
-function separaPartes(str){
+    contentInicial = transMaiusc(contentInicial);
+    document.getElementById("demo2").innerHTML = "transMaiusc: " + '<br>' +  contentInicial;
 
-    var parts = str.match(/.{1,2}/g) || [];
-    return parts;
+    contentInicial = removeEspacos(contentInicial);
+    document.getElementById("demo3").innerHTML = "removeEspacos: " + '<br>' + contentInicial;
+
+    var content1 = separaPartes(contentInicial);
+    document.getElementById("demo4").innerHTML = "separaPartes: " + '<br>' +  content1;
+
+    var content2 = separaPartes(contentInicial.substring(1));
+    document.getElementById("demo5").innerHTML = "separaPartes2: "  + '<br>' +  content2;
+    
+    contentInicial = content1.concat(content2);
+    document.getElementById("demo6").innerHTML = "concat: "  + '<br>' + contentInicial;
+
+    var content3 = contentInicial.filter( onlyUnique );
+    document.getElementById("demo7").innerHTML = "onlyUnique: "  + '<br>' + content3;
+
+    var contentFinal2 = percorreLista(contentInicial, content3);
+    document.getElementById("demo10").innerHTML = "percorreLista2: " + contentFinal2; 
+    
+    //Finaliza a contagem de tempo
+    t1 = performance.now();
+    document.getElementById("demo11").innerHTML = "O processo levou " + (t1 - t0) + " milliseconds.";
 }
 
 function removeAcentos (str){
@@ -86,30 +78,32 @@ function transMaiusc (str){
     return str.toUpperCase();
 }
 
-function percorreLista (str,stringsearch){
-
-    var text = "";
-
-    for(var i = 0; i < stringsearch.length; i++) {
-        
-        text += contaOcorrencias(str, stringsearch[i]);
-    }
-
-    return text;
-
+function removeEspacos (str){
+    str = str.replace(/\s/g, '');
+    return str;
 }
 
-//https://www.javascripttutorial.net/javascript-multidimensional-array/
-//https://www.geeksforgeeks.org/how-to-create-two-dimensional-array-in-javascript/
-function percorreLista2 (str,stringsearch){
+function separaPartes(str){
 
+    var parts = str.match(/.{1,2}/g) || [];
+    return parts;
+}
+
+function onlyUnique(value, index, self) { 
+    return self.indexOf(value) === index;
+}
+
+function percorreLista (str,stringsearch){
+
+    //Declara a lista de duas dimenções
     var activities = new Array(2);
 
-    // Loop to create 2D array using 1D array 
+    //Inicializa o array
     for (var i = 0; i < str.length; i++) { 
         activities[i] = new Array(2); 
     }
 
+    //Preenche o array
     var total = 0;
     for(var i = 0; i < stringsearch.length; i++) {
         for(var j=count=0; j<str.length; count+=+(stringsearch[i]===str[j++])){
@@ -119,24 +113,18 @@ function percorreLista2 (str,stringsearch){
         };
     }
 
-    console.log(total);
+    //Ordena decrescente
+    activities.sort(function(a,b){ return b[1] - a[1]; });
 
-    //ordena decrescente
-    activities.sort(function(a,b){
-        return b[1] - a[1];
-   });
-
+    //Calcula as porcentagens
     for (var i = 0; i < activities.length; i++) {
         var percentage = ((activities[i][1] / total) * 100);
         activities[i][2] = percentage;
-        //console.log(activities[i][2]);
     }
 
+    //Cria a tabela
     var text = "<table>";
-    // for(var i = 0; i < stringsearch.length; i++) {
-    //     text+= activities[i][0] + " " + activities[i][1] + '<br>'
-    // }
-
+    //Monta cabeçalho da tabela
     text+= 
     '<tr>' + 
         '<th>' + "Indice" + '</th>' +
@@ -145,17 +133,10 @@ function percorreLista2 (str,stringsearch){
         '<th>' + "Porcentagem" + '</th>' +
     '</tr>';
 
+    //monta estrutura da tabela
     for(var i = 1; i < 101; i++) {
         text+= '<tr>' + '<td>' + i + '</td>' + '<td>' + activities[i][0] + '</td>'+ '<td>' + activities[i][1] + '</td>' + '<td>' + activities[i][2].toFixed(5).slice(0,-1) + " %" + '</td>' + '</tr>';
     }
 
     return text;
 }
-
-
-function onlyUnique(value, index, self) { 
-    return self.indexOf(value) === index;
-}
-
-//Colocar em ordem
-//https://www.javascripttutorial.net/javascript-multidimensional-array/
